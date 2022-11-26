5a19e4b97d1128aba313ac8d550541ddab2c511a
pj-web <insta.pj.05@gmail.com>
2022-11-26 22:21:18 +0400

    Lesson 2 complete

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,22 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.8;
 
+// EVM, Ethereum Virtual Machine
+// Avalanche, Fantom, Polygon
+
 contract SimpleStorage {
     // boolean, uint, int, address, bytes
 
     // This get initialized to zero!
     uint256 public favoriteNumber;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
     People public person = People({favoriteNumber: 2, name: "Patric"});
 
     struct People {
         uint256 favoriteNumber;
-        string name;
-        
+        string name;   
     }
+
+    People[] public people;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
         favoriteNumber = favoriteNumber + 1;
+    }
+
+    // view, pure
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+
+    // calldata, memory, storage
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
 
