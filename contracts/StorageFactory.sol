c6331d9543f12d602b05a2cf96f7f5c3193cf4b0
pj-web <insta.pj.05@gmail.com>
2022-11-27 22:12:50 +0400

    modify StorageSactory

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -9,6 +9,17 @@ contract StorageFactory {
     function createSimpleStorageContract() public {
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
+    }
 
+    function sfStorage(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Address
+        // ABI - Aplication Binary Interface
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retrieve();
     }
 }
