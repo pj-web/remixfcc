d2249640d23fd68dfa33799059cfebaa639f15b6
pj-web <insta.pj.05@gmail.com>
2022-11-30 19:16:08 +0400

    +PriceConverter

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,8 +5,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-
 contract FundMe {
 
     uint256 public minimumUsd = 50 * 1e18; // 1 * 10 ** 18
@@ -26,28 +24,7 @@ contract FundMe {
         // Undo any action before, and send remaning gas back
     }
 
-    function getPrice() public view returns (uint256) {
-        // ABI
-        // Address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        // ETH in terms of USD
-        // 3000.00000000
-        return uint256 (price * 1e10); // 1*10 == 10000000000
-    }
-
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        return priceFeed.version();
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        // 2.999e21 = 2999.999999999999999999
-        // 3000
-        return ethAmountInUsd;
-    }
+ 
 
 
 
--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -2,6 +2,29 @@
 
 pragma solidity ^0.8.0;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 library PriceConverter {
-    
+       function getPrice() public view returns (uint256) {
+        // ABI
+        // Address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        // ETH in terms of USD
+        // 3000.00000000
+        return uint256 (price * 1e10); // 1*10 == 10000000000
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        return priceFeed.version();
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        // 2.999e21 = 2999.999999999999999999
+        // 3000
+        return ethAmountInUsd;
+    }
 }
