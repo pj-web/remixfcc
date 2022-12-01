1fd0b90b8084ead5287e56a97b666e6cc79a26ac
pj-web <insta.pj.05@gmail.com>
2022-12-01 15:37:33 +0400

    20221201

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,7 +5,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import "./PriceConverter.sol";
+
 contract FundMe {
+    using PriceConverter for uint256;
 
     uint256 public minimumUsd = 50 * 1e18; // 1 * 10 ** 18
 
@@ -15,7 +18,7 @@ contract FundMe {
     function fund() public payable {
         // Want to set a minimum fund amount in USD
         // 1. How do we send ETH to this contract?
-        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enought"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enought"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000
         // 18 decimals
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-       function getPrice() public view returns (uint256) {
+       function getPrice() internal view returns (uint256) {
         // ABI
         // Address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
@@ -15,12 +15,12 @@ library PriceConverter {
         return uint256 (price * 1e10); // 1*10 == 10000000000
     }
 
-    function getVersion() public view returns (uint256) {
+    function getVersion() internal view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
         return priceFeed.version();
     }
 
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+    function getConversionRate(uint256 ethAmount) internal view returns(uint256) {
         uint256 ethPrice = getPrice();
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
         // 2.999e21 = 2999.999999999999999999
